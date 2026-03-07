4b22945326004b0a9771bab5965137f69998755e
drianto <andri070805@gmail.com>
2026-03-07 22:13:08 +0700

    feat: add activation function

--- src/activation.py
+++ src/activation.py
@@ -1,1 +1,17 @@
 # File untuk menyimpan perhitungan fungsi aktivasi
+import numpy as np
+
+def linear(x):
+    return np.x
+
+def relu(x):
+    return max(0, np.x)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def hyperbolic_tangent(x):
+    return np.tanh(x)
+
+def softmax(x):
+    return np.exp(x) / np.exp(x).sum()
--- src/ffnn.py
+++ src/ffnn.py
@@ -1,15 +1,16 @@
+import numpy as np
 class FFNN:
-    def __init__(self, loss, n_hidden_layer, activation, batch, learning_rate):
+    def __init__(self, loss: str, n_hidden_layer: int, activation: str, batch: int, learning_rate: float) -> None:
         self.loss = loss
         self.n_hidden_layer = n_hidden_layer
         self.activation = activation
         self.batch = batch
         self.learning_rate = learning_rate
 
-    def fit(self, X, y):
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         pass
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
         pass
 
 def main():
